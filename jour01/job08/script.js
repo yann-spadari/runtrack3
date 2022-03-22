6acac11f6c160c87739e5bc67a4d0a68f0205d9e
Yann Spadari <91940990+yann-spadari@users.noreply.github.com>
2022-03-22 17:32:02 +0100

    Job08

--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -1,1 +1,37 @@
-...
+
+function sommenombrespremiers(a, b) {
+
+    // check if number is equal to 1
+    if (a > 1 && b > 1) {
+
+        for (let i = 2; i < a; i++) {
+
+            if (a % i == 0) {
+
+                break;
+
+            } else {
+                for (let j = 2; j < a; j++) {
+
+                    if (b % j == 0) {
+
+                        break;
+                        
+                    } else {
+
+                        console.log(a + b);
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+    } else {
+
+        console.log('false');
+        
+    }
+
+}
