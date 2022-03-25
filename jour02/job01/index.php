354aa65e97a0dcd88ca434a70f9af40b677dbebd
Yann Spadari <91940990+yann-spadari@users.noreply.github.com>
2022-03-25 13:31:18 +0100

    Jour 2 - Runtrack JS

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,17 +1,12 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="script.js" defer></script>
-    <title>Job 08</title>
+    <title>Job 01</title>
+    <script type="text/javascript" src="script.js"></script>
 </head>
-
 <body>
-<p id="demo"></p>
-
 </body>
-
 </html>
